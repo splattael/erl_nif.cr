4f632e64fdabe603dc885df0bb82d7d5cac365fe
Peter Suschlik <peter@suschlik.de>
2016-08-04 22:46:56 +0200

    Remove unneeded callback functions

--- src/hello_world.cr
+++ src/hello_world.cr
@@ -30,34 +30,12 @@ fun nif_init : ErlNif::EntryT*
     echo_func
   ]
 
-  load = ->(env : ErlNif::Nifenv, priv : Void**, load_info : ErlNif::NifTerm) {
-    p "LOAD"
-    0
-  }
-
-  reload = ->(env : ErlNif::Nifenv, priv : Void**, load_info : ErlNif::NifTerm) {
-    p "RELOAD"
-    0
-  }
-
-  upgrade = ->(env : ErlNif::Nifenv, priv : Void**, old_priv : Void**, load_info : ErlNif::NifTerm) {
-    p "UPGRADE"
-    0
-  }
-
-  unload = ->(env : ErlNif::Nifenv, priv : Void*) {
-    p "UNLOAD"
-  }
-
   entry = ErlNif::EntryT.new(
     major: ErlNif::NIF_MAJOR_VERSION,
     minor: ErlNif::NIF_MINOR_VERSION,
     name: "Elixir.HelloWorld",
     num_of_funcs: funcs.size,
     funcs: funcs,
-    reload: reload,
-    upgrade: upgrade,
-    unload: unload,
     vm_variant: "beam.vanilla",
     options: ErlNif::NIF_DIRTY_NIF_OPTION
   )
